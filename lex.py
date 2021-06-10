59c8465132d724a22ff14f2ef8d73ab7c21516ff
vzazueta <A00226702@itesm.mx>
2021-06-10 15:39:23 -0700

    yacc.py closer to completeion; main.py created to unite everything

--- lex.py
+++ lex.py
@@ -1,85 +1,108 @@
 from ply import lex
 
-tokens = (
-    'ADD',
-    'SUBSTRACT',
-    'MULTIPLY',
-    'DIVIDE',
-    'EXP',
-    'EQUAL',
-    'NOT_EQUAL',
-    'GREATER',
-    'LESS',
-    'GREATER_EQUAL',
-    'LESS_EQUAL',
-    'AND',
-    'OR',
-    'NUMBER',
-    'LPAREN',
-    'RPAREN',
-    'LBRACE',
-    'RBRACE',
-    'SEMICOLON',
-    'ID',
-)
 
-reserved = {
-   'if' : 'IF',
-   'elif' : 'ELIF',
-   'else' : 'ELSE',
-   'then' : 'THEN',
-   'while' : 'WHILE',
-   'do' : 'DO',
-   'for' : 'FOR',
-   'and' : 'AND',
-   'or' : 'OR',
-   'true' : 'TRUE',
-   'false' : 'FALSE',
-   'int' : 'INTEGER',
-   'float' : 'FLOAT',
-   'boolean' : 'BOOLEAN',
-   'string' : 'STRING',
-}
+class Lexer:
 
-t_ADD = r'\+'
-t_SUBSTRACT = r'\-'
-t_MULTIPLY = r'\*'
-t_DIVIDE = r'/'
-t_EXP = r'\^'
-t_EQUAL = r'\=\='
-t_NOT_EQUAL = r'\!\='
-t_GREATER = r'\>'
-t_LESS = r'\<'
-t_GREATER_EQUAL = r'\>\='
-t_LESS_EQUAL = r'\<\='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LBRACE  = r'\{'
-t_RBRACE  = r'\}'
-t_SEMICOLON = r'\;'
-t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-t_NUMBER = r'\d+'
+    def __init__(self):
+        self.errors = []
 
-t_ignore = '\t'
+    
+    reserved = {
+    'if' : 'IF',
+    'elif' : 'ELIF',
+    'else' : 'ELSE',
+    'then' : 'THEN',
+    'while' : 'WHILE',
+    'do' : 'DO',
+    'for' : 'FOR',
+    'and' : 'AND',
+    'or' : 'OR',
+    'true' : 'TRUE',
+    'false' : 'FALSE',
+    'int' : 'INTEGER',
+    'float' : 'FLOAT',
+    'boolean' : 'BOOLEAN',
+    'string' : 'STRING',
+    }
 
-def t_id( t ):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')
-    return t
+    literals = ['=', '+', '-', '*', '/', '^',
+                ">", "<", '(', ')', '{', '}', ';']
 
-def t_number( t ):
-    r'\d+'
-    try:
-        t.lexer.num_count += 1
+    tokens = [
+        'ADD',
+        'SUBSTRACT',
+        'MULTIPLY',
+        'DIVIDE',
+        'EXP',
+        'EQUAL',
+        'NOT_EQUAL',
+        'GREATER',
+        'LESS',
+        'GREATER_EQUAL',
+        'LESS_EQUAL',
+        'AND',
+        'OR',
+        'NUMBER',
+        'LPAREN',
+        'RPAREN',
+        'LBRACE',
+        'RBRACE',
+        'SEMICOLON',
+        'ID',
+    ] + list(reserved.values())
+
+    t_ADD = r'\+'
+    t_SUBSTRACT = r'\-'
+    t_MULTIPLY = r'\*'
+    t_DIVIDE = r'/'
+    t_EXP = r'\^'
+    t_EQUAL = r'=='
+    t_NOT_EQUAL = r'!='
+    t_GREATER = r'\>'
+    t_LESS = r'\<'
+    t_GREATER_EQUAL = r'>='
+    t_LESS_EQUAL = r'<='
+    t_LPAREN  = r'\('
+    t_RPAREN  = r'\)'
+    t_LBRACE  = r'\{'
+    t_RBRACE  = r'\}'
+    t_SEMICOLON = r'\;'
+    t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t_NUMBER = r'\d+'
+
+    t_ignore = ' \t'
+    t_ignore_COMMENT = r'\/\/.*'
+
+    def t_id(self, t):
+        r'[a-zA-Z_][a-zA-Z0-9_]*'
+        t.type = reserved.get(t.value, 'ID')
+        return t
+
+    def t_int(self, t):
+        r'[0-9]+'
         t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %d", t.value)
-        t.value = 0
-    return t
+        return t
 
-def t_error( t ):
-    print("Error: %s \n" % t.value[0])
-    t.lexer.skip(1)
+    def t_float(self, t):
+        r'[0-9]*\.[0-9]+'
+        t.value = float(t.value)
+        return t
+
+    def t_string(self, t):
+        r'".*"'
+        return t
+
+    def t_newline(self, t):
+        r'\n+|\r+|(\r\n)+'
+        t.lexer.lineno += len(t.value)
+
+    def t_error(self, t):
+        print("Error: %s \n" % t.value[0])
+        t.lexer.skip(1)
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        return self.lexer
 
 '''
 def t_eof(t):
@@ -91,17 +114,11 @@ def t_eof(t):
     return None
 '''
 
-data = '''
-3 + 4 * 10
-'''
+'''lexer.num_count = 0
+lexer.input(data)'''
 
-lexer = lex.lex()
-
-lexer.num_count = 0
-lexer.input(data)
-
-while True:
+'''while True:
     tok = lexer.token()
     if not tok:
         break
-    print(tok)
+    print(tok)'''
--- yacc.py
+++ yacc.py
@@ -1,78 +1,135 @@
 import ply.yacc as yacc
-from lex import tokens, reserved
+from lex import Lexer
 
-precedence = (
-    ('right', '='),
-    ('left', 'AND', 'OR'),
-    ('left', 'EQUALS'),
-    ('nonassoc', '<', '>', 'GREAT_EQUAL', 'LESS_EQUAL'),
-    ('left', 'ADD', 'SUBSTRACT'),
-    ('left', 'MULTIPLY', 'DIVIDE'),
-    ('left', 'EXP'),
-)
+class Symbol:
 
-def p_expression_binop( p ):
-    '''expr : expr ADD expr
-            | expr SUBSTRACT expr
-            | expr MULTIPLY expr
-            | expr DIVIDE expr
-            | expr EXP expr'''
-    if p[2] == '+'  : p[0] = p[1] + p[3]
-    elif p[2] == '-': p[0] = p[1] - p[3]
-    elif p[2] == '*': p[0] = p[1] * p[3]
-    elif p[2] == '/': p[0] = p[1] / p[3]
-    elif p[2] == '^': p[0] = p[1] ** p[3]
+    def __init__(self, name, type, declaration_pos, value=None, init_pos=None):
+        self.name = name
+        self.type = type.upper()
+        self.declaration_pos = declaration_pos
+        self.value = value
+        self.init_pos = init_pos
 
-def p_parens( p ):
-    'parens : LPAREN condition RPAREN'
-    p[0] = p[2]
+    def initialized(self):
+        return self.init_pos is not None
 
-def p_braces( p ):
-    'braces : LBRACE statement RBRACE'
-    p[0] = p[2]
-    return p[0]
 
-def p_special_statement( p ):
-        ''' special_statement : parens braces'''
-        p[0] = (p[1], p[2])
+class Yaccer:
 
-def p_expression_to_number( p ) :
-    'num : NUMBER'
-    p[0] = p[1]
-    return p[0]
+    tokens = Lexer.tokens
+    start = 'prog'
+    precedence = (
+        ('left', 'ADD', 'SUBSTRACT'),
+        ('left', 'MULTIPLY', 'DIVIDE'),
+        ('left', 'EXP'),
+    )
 
-def p_conditional( p ):
-    '''expr : IF special_statement
-            | IF special_statement ELSE braces
-            | IF special_statement elif ELSE braces'''
+    def __init__(self):
+        self.errors = []
+        self.lexer = Lexer()
+        self.lexer = self.lexer.build()
+        self.symbol_table = {}
+
+    def p_prog(self, p):
+        ''' prog : prog expression
+                 | expression '''
+        if len(p) == 3 and p[2][0] is not None:
+            p[0] = p[1] + (p[2],)
+        else:
+            p[0] = (p[1],)
+
+    def p_type(self, p):
+        ''' type : INT
+                 | FLOAT
+                 | STRING
+                 | BOOLEAN '''
+        p[0] = p[1]
+
+    def p_expression_binop(self, p):
+        '''operation : operation ADD operation
+                    | operation SUBSTRACT operation
+                    | operation MULTIPLY operation
+                    | operation DIVIDE operation
+                    | operation EXP operation'''
+        if p[2] == '+'  : p[0] = p[1] + p[3]
+        elif p[2] == '-': p[0] = p[1] - p[3]
+        elif p[2] == '*': p[0] = p[1] * p[3]
+        elif p[2] == '/': p[0] = p[1] / p[3]
+        elif p[2] == '^': p[0] = p[1] ** p[3]
+
+    def p_declaration(self, p):
+        ''' declaration : type ID
+                        | type ID EQUALS operation'''
+
+    def p_parens(self, p):
+        'parens : LPAREN condition RPAREN'
+        p[0] = p[2]
+
+    def p_braces(self, p):
+        'braces : LBRACE statement RBRACE'
+        p[0] = p[2]
+        return p[0]
+
+    def p_special_statement(self, p):
+            ''' special_statement : parens braces'''
+            p[0] = (p[1], p[2])
+
+    def p_expression_to_number(self, p) :
+        'num : NUMBER'
+        p[0] = p[1]
+        return p[0]
+
+    def p_conditional(self, p):
+        '''expr : IF special_statement
+                | IF special_statement ELSE braces
+                | IF special_statement elif ELSE braces'''
+        
+        if len(p) == 3:
+            p[0] = (p[1], p[2], None, None)
+        elif len(p) == 5:
+            p[0] = (p[1], p[2], None, (p[3], p[4]))
+        else:
+            p[0] = (p[1], p[2], p[3], (p[4], p[5]))
+
+    def p_elif(self, p):
+        '''elif : ELIF special_statement
+                | elif ELIF special_statement '''
+        
+        if p[1] == 'elif':
+            p[0] = ((p[1], p[2][0], p[2][1]),)
+        else:
+            p[0] = p[1] + ((p[2], p[3][0], p[3][1]),)
+
+    def p_iteration(self, p):
+        ''' iteration : WHILE parens 
+                    | DO braces WHILE parens'''
+
+
+    def p_closing_statement(self, p):
+        ''' closing : ';'
+                    | statement ';' '''
+        if p[1] != ";":
+            p[0] = p[1]
+
+    def p_statement(self, p):
+        ''' statement : operation
+                      | declaration '''
+        p[0] = p[1]
+
+    def p_error(self, p):
+        if p:
+            self.errors.append(
+                "Syntax error: Symbol {}; line {}".format(p.value, p.lineno))
+        else:
+            self.errors.append(
+                "Unexpected EOF")
+
     
-    if len(p) == 3:
-        p[0] = (p[1], p[2], None, None)
-    elif len(p) == 5:
-        p[0] = (p[1], p[2], None, (p[3], p[4]))
-    else:
-        p[0] = (p[1], p[2], p[3], (p[4], p[5]))
+    def build(self, **kwargs):
+        self.parser = yacc.yacc(module=self, **kwargs)
+        return self.parser
 
-def p_elif( p ):
-    '''elif : ELIF special_statement
-            | elif ELIF special_statement '''
-    
-    if p[1] == 'elif':
-        p[0] = ((p[1], p[2][0], p[2][1]),)
-    else:
-        p[0] = p[1] + ((p[2], p[3][0], p[3][1]),)
-
-def p_iteration( p ):
-    ''' iter : WHILE parens '''
-
-def p_error( p ):
-    print("Syntax error at '%s'" % p.value)
-
-data = '''
-3 + 4 * 10
-'''
-
-parser = yacc.yacc()
+'''parser = yacc.yacc()
 
 res = parser.parse(data)
-print(res)
+print(res)'''
